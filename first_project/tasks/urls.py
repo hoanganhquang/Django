9c02866dccf87904592587ea45a612a27945b458
hoanganhquang <anhquang976@gmail.com>
2021-07-30 16:16:11 +0700

    Updated tasks app

--- first_project/tasks/urls.py
+++ first_project/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("task", add_task, name="add_task")
 ]
--- first_project/tasks/views.py
+++ first_project/tasks/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+tasks = ["1", "2", "3"]
+
 
 def index(request):
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+
+def add_task(request):
+    return render(request, "tasks/add.html")
