722f4d3c3de6d0d7d6b63da61a709584c4c8c451
hoanganhquang <anhquang976@gmail.com>
2021-07-30 11:30:54 +0700

    Add app tasks

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hello',
     'new_year',
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
-    path('new-year/', include('new_year.urls'))
+    path('new-year/', include('new_year.urls')),
+    path('tasks/', include('tasks.urls'))
 ]
