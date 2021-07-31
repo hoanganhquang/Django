f0bf9d309f6b97c8fe66d0d4b732ca94ad4b353c
hoanganhquang <anhquang976@gmail.com>
2021-07-31 16:34:19 +0700

    Update index page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.css_page, name="css_page")
+    path("<str:name>", views.md_page, name="md_page")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
 
 def index(request):
+    if request.method == "POST":
+        name = request.POST.get('q')
+
+        return redirect('encyclopedia:md_page', name=name)
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 
-def css_page(request, name):
-    return render(request, "encyclopedia/csspage.html", {
+def md_page(request, name):
+    return render(request, "encyclopedia/md-page.html", {
         "name": util.get_entry(name)
     })
