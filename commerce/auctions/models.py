4e3b8c98c3a2054a1079588ba8b24f3f1b46aa64
hoanganhquang <anhquang976@gmail.com>
2021-08-11 17:07:22 +0700

    updated project

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class AuctionListings(models.Model):
+    item_name = models.CharField(max_length=20)
+    price = models.FloatField()
+    seller = models.CharField(max_length=10)
+    date = models.DateTimeField()
+    describe = models.CharField(max_length=50)
+
+
+class Bids(models.Model):
+    # price = models.FloatField()
+    # user = models.ForeignKey()
+    # item =
+    pass
+
+
+class Comments(models.Model):
+    pass
